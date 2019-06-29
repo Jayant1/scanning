e04618369ccfe069520cd0d95c32bf3b5605aced
Jayant1 <jay-24@hotmail.com>
2019-06-29 11:50:49 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,29 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<div class="scanner">
+    <h2>Scan de code</h2>
+</div>
+
+<video autoplay></video>
+
+<script>
+const constraints = {
+  video: true
+};
+
+const video = document.querySelector('video');
+
+navigator.mediaDevices.getUserMedia(constraints).
+  then((stream) => {video.srcObject = stream});
+</script>
+
+
+
+</body>
+</html>
